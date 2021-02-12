27ba9a84f86e7ac2461eb247bf26fb977fabee05
Pawan <p.k.shaw@wsntechnology.com>
2021-02-12 14:42:37 +0530

    My First Commit All

---
+++ Core_Java_Logical/src/main/java/com/shukla/reverseString/ReverseString.java
@@ -0,0 +1,41 @@
+package com.shukla.reverseString;
+
+public class ReverseString {
+
+	public String method1(String str) {
+
+		StringBuffer sf = new StringBuffer(str);
+		StringBuffer revString = sf.reverse();
+		return revString.toString();
+
+	}
+
+	public void method2(String str) {
+
+		char[] charArray = str.toCharArray();
+		for (int i = charArray.length - 1; i >= 0; i--) {
+			System.out.print(charArray[i]);
+		}
+	}
+
+	public String method3(String str) {
+
+		String revString = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			revString = revString + str.charAt(i);
+		}
+		return revString;
+
+	}
+
+	public static void main(String[] args) {
+
+		String method1 = new ReverseString().method1("shukla");
+		System.out.println(method1);
+		new ReverseString().method2("shukla");
+		String method3 = new ReverseString().method3("shukla");
+		System.out.println(method3);
+
+	}
+
+}
---
+++ Core_Java_Logical/src/test/java/com/shukla/CoreJavaLogicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shukla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreJavaLogicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
